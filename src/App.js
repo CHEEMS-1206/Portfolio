95b1493cf85322c9a23e27025d5b64efa7af2df0
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-05-28 00:06:45 +0530

    Used react router provider for routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import routerName from './Components/Routes/Routes';
 
 function App() {
   return (
     <div>
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        Hello World
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      <RouterProvider router={routerName} />
     </div>
   );
 }
