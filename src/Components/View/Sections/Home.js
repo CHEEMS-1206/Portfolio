a1679dcbc1879025aeb6fdf6ea47e6526e6c6b06
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-05-28 00:25:56 +0530

    Created home component

---
+++ src/Components/View/Sections/Home.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "../../Styles/Home.css";
+
+import Link from "../CommonComponents/LinkComponent";
+import Menubar from "../Menubar/Menubar";
+
+function Home(props) {
+  return (
+    <>
+      <Menubar />
+      <div className="home section" id="home">
+        <div className="max-width">
+          <div className="home-content div1">
+            <div className="text-1">Hey, My name is</div>
+            <div className="text-2">Priyanshu singh</div>
+            <div className="text-3">
+              And I am a <span id="typing-1">Web Developer</span>.
+            </div>
+            <Link
+              href="mailto:priyanshusingh9291@gmail.com?subject=Greetings..."
+              id="hire-me"
+              value="Conatct Me"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
