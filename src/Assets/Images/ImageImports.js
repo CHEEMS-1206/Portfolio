616837b64747b395563d802745cc137812e8805a
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-05-30 13:46:35 +0530

    File for all image imports like logo and bg

---
+++ src/Assets/Images/ImageImports.js
@@ -0,0 +1,56 @@
+import devImage from './DeveloperImage.avif'
+// font end
+import html from './Logos/TechImages/Html.png'
+import css from "./Logos/TechImages/Css.png";
+import sass from "./Logos/TechImages/Sass.png";
+import js from "./Logos/TechImages/javascript.png";
+import bootstrap from "./Logos/TechImages/Bootstrap.png";
+import react from './Logos/TechImages/React.png'
+
+// databases
+import mongodb from './Logos/TechImages/mongoDb.png'
+import mysql from "./Logos/TechImages/mysql.png";
+import postgresql from "./Logos/TechImages/postgre.png";
+
+//backend
+import nodejs from './Logos/TechImages/nodeJS.png'
+import expressjs from './Logos/TechImages/expressJS.png'
+import jenkins from './Logos/TechImages/jenkins.jpg'
+import docker from "./Logos/TechImages/docker.png";
+import redis from "./Logos/TechImages/redis.png";
+import selenium from "./Logos/TechImages/selenium.png";
+
+// vsc and deployement
+import gitGithub from "./Logos/TechImages/github.png";
+import linux from "./Logos/TechImages/linux.png";
+import aws from "./Logos/TechImages/aws.png";
+import firebase from "./Logos/TechImages/firebase.png";
+
+const ImageImports = {
+    DeveloperImage : devImage,
+
+    Html : html,
+    Css : css,
+    Javascript : js,
+    Sass: sass,
+    Bootstrap: bootstrap,
+    React : react,
+
+    MongoDb: mongodb,
+    MySql: mysql,
+    PostgreSql : postgresql,
+
+    NodeJS:nodejs,
+    ExpressJS:expressjs,
+    Jenkins: jenkins,
+    Selenium : selenium,
+    Redis : redis,
+    Docker : docker,
+
+    Github : gitGithub,
+    Linux:linux,
+    AWS : aws,
+    Firebase : firebase
+}
+
+export default ImageImports;
