110e716243b8add0a732810538a696ee2aa7c6b3
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-05-30 10:34:16 +0530

    Added the fontawesome in react file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import routerName from './Components/Routes/Routes';
 
+// font awesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+  fas,
+  // faTwitter,
+  // faFontAwesome,
+  // faHatCowboy,
+  // faHatChef,
+  // faPlateUtensils
+);
+
+
 function App() {
   return (
     <div>
